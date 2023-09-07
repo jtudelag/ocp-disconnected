be7e233fd907e978a5474e672566ac7021454bf9
Jorge <jtudelag@redhat.com>
2023-09-07 05:17:38 -0400

    Add verbosity 3

--- oc-mirror/mirror-content.sh
+++ oc-mirror/mirror-content.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 #
 
-time oc-mirror --config imageset-config-test.yaml file://mirror
+#time oc-mirror -v 3 --config imageset-config-test.yaml file://mirror
+
+time oc-mirror -v 3 --config imageset-config.yaml file://mirror
 
 #oc mirror -v 9 --from=mirror/mirror_seq1_000000.tar docker://localhost:5000
 
