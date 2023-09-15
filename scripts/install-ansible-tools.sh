375ad6162ea51dcd915f9fe7b8433f703c158937
Jorge Tudela <jtudelag@redhat.com>
2023-09-15 07:40:40 -0400

    Adds ansible tools script

---
+++ scripts/install-ansible-tools.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -x
+
+source ocp_vars.sh
+
+sudo subscription-manager repos --enable ansible-automation-platform-2.4-for-rhel-8-x86_64-rpms
+
+sudo dnf install -y ansible-core ansible-navigator ansible-builder
+
+TOKEN="$(cat $ANSIBLE_AUTOMATION_HUB_TOKEN_FILE)"
+
+cat <<EOF > ./ansible.cfg
+[defaults]
+interpreter_python=/usr/bin/python3
+
+[galaxy]
+server_list = ansible_automation_hub, galaxy
+
+[galaxy_server.ansible_automation_hub]
+url=https://console.redhat.com/api/automation-hub/content/published/
+auth_url=https://sso.redhat.com/auth/realms/redhat-external/protocol/openid-connect/token
+token= '${TOKEN}'
+
+[galaxy_server.galaxy]
+url=https://galaxy.ansible.com/
+EOF
+
+
